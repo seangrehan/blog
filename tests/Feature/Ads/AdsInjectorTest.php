3122d510df130d5c4d1a424312876d66154b71d2
Sean Grehan <seangrehan13@outlook.com>
2021-09-04 11:28:52 +0100

    Create failing test, inject article has duplicate keys

--- tests/Feature/Ads/AdsInjectorTest.php
+++ tests/Feature/Ads/AdsInjectorTest.php
@@ -54,6 +54,28 @@ class AdsInjectorTest extends TestCase
      * @uses BusinessLogic\Ads\Widgets\Paragraph::getPointsValue
      * @uses BusinessLogic\Ads\Widgets\RelatedArticles::getPointsValue
      */
+    public function testInjectArticleNoDuplicateKeys(): void
+    {
+        $article = $this->repository->getArticle(1);
+        $advert = [['layout' => 'ad']];
+        $points = 3.5;
+
+        $article = $this->adsInjector->inject($article, $advert, $points);
+
+        foreach ($article['widgets'] as $key => $widget) {
+            if ($key !== 0) {
+                $this->assertNotEquals($article['widgets'][$key - 1], $widget);
+            }
+        }
+    }
+
+    /**
+     * @uses BusinessLogic\Repository\Repository::getArticle
+     * @uses BusinessLogic\Ads\Widgets\WidgetFactory::create
+     * @uses BusinessLogic\Ads\Widgets\Embed::getPointsValue
+     * @uses BusinessLogic\Ads\Widgets\Paragraph::getPointsValue
+     * @uses BusinessLogic\Ads\Widgets\RelatedArticles::getPointsValue
+     */
     public function testInjectNoWidgets(): void
     {
         $article = $this->repository->getArticle(4);
